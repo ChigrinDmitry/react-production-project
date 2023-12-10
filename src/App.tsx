ceccf06812801175de4d4b9662386913d1d6879d
Dmitry Chigrin <chigrin.dmitry@gmail.com>
2023-12-10 23:25:05 +0500

    feat: add LIGHT and DARK themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext, useState } from 'react';
 import Counter from './components/Counter';
 import './styles/index.scss'
 import { Route, Routes } from 'react-router-dom';
@@ -7,10 +7,15 @@ import AboutPage from './pages/AboutPage/AboutPage';
 import { Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { ThemeContext, Theme } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
         <Suspense fallback={<div>Loading ...</div>}>      
